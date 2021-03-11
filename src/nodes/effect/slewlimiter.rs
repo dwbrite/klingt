66f0739ac66c1268ebfdb47625dba7ca25a1b755
Devin Brite <devin@dwbrite.com>
2021-03-11 14:12:14 -0500

    fix UB in slewlimiter

--- src/nodes/effect/slewlimiter.rs
+++ src/nodes/effect/slewlimiter.rs
@@ -2,26 +2,20 @@ use dasp_graph::{Buffer, Input, Node};
 
 // TODO: find out if I'm doing myself any good by using a raw pointer instead of a smart pointer
 pub struct SlewLimiter {
-    channels_last_ptr: *mut f32,
-    channels_last_len: usize,
+    channel_last: [f32; 8],
 }
 
 impl SlewLimiter {
     // TODO: parameterize sample rate, channels, calculate delta
     pub fn new() -> Self {
         SlewLimiter {
-            channels_last_ptr: [0f32; 5].as_mut_ptr(),
-            channels_last_len: 5,
+            channel_last: [0f32; 8],
         }
     }
 }
 
 impl Node for SlewLimiter {
     fn process(&mut self, inputs: &[Input], output: &mut [Buffer]) {
-        let slice = unsafe {
-            std::slice::from_raw_parts_mut(self.channels_last_ptr, 4 * self.channels_last_len)
-        };
-
         // Sum the inputs onto the output.
         for (channel, out_buffer) in output.iter_mut().enumerate() {
             // only accepts one input
@@ -30,12 +24,12 @@ impl Node for SlewLimiter {
             if let Some(in_buffer) = in_buffers.get(channel) {
                 for (i, o) in in_buffer.iter().zip(out_buffer.iter_mut()) {
                     // TODO: better math
-                    let last = slice[channel];
+                    let last = self.channel_last[channel];
                     let delta = i - last;
                     let min_per_sample = 1_f32 / 16_f32;
 
                     *o = last + delta.abs().min(min_per_sample).copysign(delta);
-                    slice[channel] = *o;
+                    self.channel_last[channel] = *o;
                 }
             }
         }
